6c21533f8ef50cf34fcf33b441245ca39124c211
Ezra Prince Situma <s.prince2@alustudent.com>
2026-06-29 23:56:13 +0300

    Added solutions for Level 2 and Level 5 quests

---
+++ quests/quest_22_personalized_scroll.py
@@ -0,0 +1,7 @@
+def personalized_greeting(name, quest):
+    print(f"Hello {name}! Your quest is to {quest}. Best of luck!")
+
+name = input("Enter your name: ")
+quest = input("Enter your quest: ")
+
+personalized_greeting(name, quest)
---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,8 @@
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 4)
+area2 = calculate_area(10, 8)
+
+print(f"The area of the first rectangle is {area1}.")
+print(f"The area of the second rectangle is {area2}.")
